21914db11fb04d21efccc172e6f87c7dea32f4dc
abimael <abibarre.82@gmail.com>
2020-03-05 13:57:37 -0500

    Adds lavalink and play function(working)

--- bot.js
+++ bot.js
@@ -5,24 +5,30 @@ const { ErelaClient } = require("erela.js");
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const nodes = [{
-  host: "localhost",
-  port: 2333,
-  password: "youshallnotpass",
-}]
+const nodes = [
+  {
+    host: "localhost",
+    port: 2333,
+    password: "youshallnotpass"
+  }
+];
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
-  //Erila stuff 
+  //Erila stuff
   client.music = new ErelaClient(client, nodes);
   // Listens to events.
   client.music.on("nodeConnect", node => console.log("New node connected"));
-  client.music.on("nodeError", (node, error) => console.log(`Node error: ${error.message}`));
-  client.music.on("trackStart", (player, track) => player.textChannel.send(`Now playing: ${track.title}`));
+  client.music.on("nodeError", (node, error) =>
+    console.log(`Node error: ${error.message}`)
+  );
+  client.music.on("trackStart", (player, track) =>
+    player.textChannel.send(`Now playing: ${track.title}`)
+  );
   client.music.on("queueEnd", player => {
-      player.textChannel.send("Queue has ended.")
-      client.music.players.destroy(player.guild.id);
+    player.textChannel.send("Queue has ended.");
+    client.music.players.destroy(player.guild.id);
   });
 });
 
@@ -41,73 +47,45 @@ client.on("guildCreate", rdy => {
 client.on("message", msg => {
   if (msg.author.bot) return;
 
-  if(msg.content === ".play"){
-    play(client,msg,msg.content);
-  }
+  play(client, msg, msg.content);
+
   if (msg.channel.name === "light") {
-    
-    //var command = commands(msg.content.split(" "));
-
-    
-     /*
-      msg.reply(command).then(bot_msg => {
-      bot_msg.delete(deletesMsg);
-    });
-  */
-
-   /* if (msg.content === "ping") {
-      msg.reply("pong").then(bot_msg => {
-        bot_msg.delete(deletesMsg);
-      });
-    
-
-    }
-    */
-    const deletesMsg = {
-      timeout: 5000,
-      reason: "none"
-    };
     msg.delete(deletesMsg);
   }
 });
 
-/*
-function commands(command){
-  var result;  
-  switch(command[0]){
-      case ".help": result = "!command";
-        break;
-      case ".play": result = "song";
-      break;
-      default: result = "Invalid command";
-    }
-    return result;
-}
-*/
+const deletesMsg = {
+  timeout: 5000,
+  reason: "none"
+};
 
-async function play (bot, message, args){
+async function play(bot, message, args) {
   const voiceChannel = message.member.voice.channel;
   console.log(voiceChannel);
-  if(!voiceChannel) return message.channel.send("you need to be in voice channel!");
+  if (!voiceChannel)
+    return message.channel.send("you need to be in voice channel!");
   if (message.content.startsWith(".play")) {
     // Spawns a player and joins the voice channel.
     const player = bot.music.players.spawn({
-        guild: message.guild,
-        voiceChannel: voiceChannel,
-        textChannel: message.channel,
+      guild: message.guild,
+      voiceChannel: voiceChannel,
+      textChannel: message.channel
     });
 
     // Searches Youtube with your query and the requester of the track(s).
     // Returns a SearchResult with tracks property.
-    const res = await bot.music.search(message.content.slice(6), message.author);
+    let link = args.split(" ");
+    console.log(link[1]);
+    const res = await bot.music.search(link[1], message.author);
 
     // Adds the first track to the queue.
     player.queue.add(res.tracks[0]);
-    message.channel.send(`Enqueuing track ${res.tracks[0].title}.`)
+    message.channel
+      .send(`Queue ${res.tracks[0].title}.`)
+      .then(songDel => songDel.delete(deletesMsg));
 
     // Plays the player (plays the first track in the queue).
     // The if statement is needed else it will play the current track again
     if (!player.playing) player.play();
+  }
 }
-}
-
