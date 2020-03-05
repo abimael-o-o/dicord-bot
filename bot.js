9533d71b68f347b3e336e6cee77b698d9c4774e4
Ramesh <st3vegaming@gmail.com>
2020-03-05 13:16:19 -0500

    Erila Stuff

--- bot.js
+++ bot.js
@@ -1,12 +1,29 @@
 // Run dotenv
 require("dotenv").config();
+const { Client } = require("discord.js");
+const { ErelaClient } = require("erela.js");
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
-let rdyGuild;
+const nodes = [{
+  host: "localhost",
+  port: 2333,
+  password: "youshallnotpass",
+}]
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  //Erila stuff 
+  client.music = new ErelaClient(client, nodes);
+  // Listens to events.
+  client.music.on("nodeConnect", node => console.log("New node connected"));
+  client.music.on("nodeError", (node, error) => console.log(`Node error: ${error.message}`));
+  client.music.on("trackStart", (player, track) => player.textChannel.send(`Now playing: ${track.title}`));
+  client.music.on("queueEnd", player => {
+      player.textChannel.send("Queue has ended.")
+      client.music.players.destroy(player.guild.id);
+  });
 });
 
 client.login(process.env.DISCORD_TOKEN);
@@ -24,15 +41,19 @@ client.on("guildCreate", rdy => {
 client.on("message", msg => {
   if (msg.author.bot) return;
 
+  if(msg.content === ".play"){
+    play(client,msg,msg.content);
+  }
   if (msg.channel.name === "light") {
     
-    var command = commands(msg.content.split(" "));
+    //var command = commands(msg.content.split(" "));
 
-
-    msg.reply(command).then(bot_msg => {
+    
+     /*
+      msg.reply(command).then(bot_msg => {
       bot_msg.delete(deletesMsg);
     });
-
+  */
 
    /* if (msg.content === "ping") {
       msg.reply("pong").then(bot_msg => {
@@ -50,6 +71,7 @@ client.on("message", msg => {
   }
 });
 
+/*
 function commands(command){
   var result;  
   switch(command[0]){
@@ -61,3 +83,31 @@ function commands(command){
     }
     return result;
 }
+*/
+
+async function play (bot, message, args){
+  const voiceChannel = message.member.voice.channel;
+  console.log(voiceChannel);
+  if(!voiceChannel) return message.channel.send("you need to be in voice channel!");
+  if (message.content.startsWith(".play")) {
+    // Spawns a player and joins the voice channel.
+    const player = bot.music.players.spawn({
+        guild: message.guild,
+        voiceChannel: voiceChannel,
+        textChannel: message.channel,
+    });
+
+    // Searches Youtube with your query and the requester of the track(s).
+    // Returns a SearchResult with tracks property.
+    const res = await bot.music.search(message.content.slice(6), message.author);
+
+    // Adds the first track to the queue.
+    player.queue.add(res.tracks[0]);
+    message.channel.send(`Enqueuing track ${res.tracks[0].title}.`)
+
+    // Plays the player (plays the first track in the queue).
+    // The if statement is needed else it will play the current track again
+    if (!player.playing) player.play();
+}
+}
+
