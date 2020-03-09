9b37c2384f29f4ffa3252e7389e2a690379c7f81
abimael <abibarre.82@gmail.com>
2020-03-09 15:52:01 -0400

    No oauth needed, working YTAPI

--- youtube_api.js
+++ youtube_api.js
@@ -1,26 +1,49 @@
-'use strict';
-
 const {google} = require('googleapis');
-const sampleClient = require('./sampleclient');
-
-// initialize the Youtube API library
+require("dotenv").config();
 const youtube = google.youtube({
   version: 'v3',
-  auth: sampleClient.oAuth2Client,
+  auth: process.env.YT_KEY // specify your API key here
 });
 
-// a very simple example of searching for youtube videos
-async function runSample() {
-  const res = await youtube.search.list({
-    part: 'id,snippet',
-    q: 'Node.js on Google Cloud',
-  });
-  console.log(res.data);
-}
+const params = {
+  part: 'snippet',
+  type: 'video',
+  maxResults: 10,
+  q: 'surf'
+};
 
-const scopes = ['https://www.googleapis.com/auth/youtube'];
+async function main(params) {
+  const res = await youtube.search.list(params);
+  console.log(res.data.items[0].id.videoId)
+};
 
-sampleClient
-  .authenticate(scopes)
-  .then(runSample)
-  .catch(console.error);
+main(params);
+
+// 'use strict';
+
+// const {google} = require('googleapis');
+// const sampleClient = require('./sampleclient');
+
+// // initialize the Youtube API library
+// const youtube = google.youtube({
+//   version: 'v3',
+//   auth: sampleClient.oAuth2Client,
+// });
+
+// // a very simple example of searching for youtube videos
+// async function runSample() {
+//   const res = await youtube.search.list({
+//     part: 'snippet',
+//     q: 'surfing',
+//     maxResults: 10,
+//     type: 'video'
+//   });
+//   console.log(res.data.items[0].id.videoId);
+// }
+
+// const scopes = ['https://www.googleapis.com/auth/youtube'];
+
+// sampleClient
+//   .authenticate(scopes)
+//   .then(runSample)
+//   .catch(console.error);
